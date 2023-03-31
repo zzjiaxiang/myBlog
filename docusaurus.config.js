17bf63ae899b6ff2f3e829bab2bf914a740b8642
张嘉祥 <jiaxiang.zhang@definesys.com>
2023-03-31 23:46:31 +0800

    fix(网站备案)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `<a href="https://beian.miit.gov.cn/" target="_blank">陕ICP备2023001423号-1</a>`,
       },
       prism: {
         theme: lightCodeTheme,
