ebab628bfdf461003333cd3288d0974a78f2c320
ZhangJiaxiang <zj.x9872@foxmail.com>
2023-07-19 17:55:46 +0800

    docs: delete config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,15 +98,15 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
           {
             title: 'Community',
             items: [
