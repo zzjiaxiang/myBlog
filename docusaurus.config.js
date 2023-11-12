7a35eff627507fd657a41e228b14225640211349
ZhangJiaxiang <zj.x9872@foxmail.com>
2023-11-13 00:37:23 +0800

    🔧 build: add dep

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
       }),
     ],
   ],
-
+  themes: ['@docusaurus/theme-live-codeblock'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
